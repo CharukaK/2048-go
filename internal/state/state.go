b6081001e3eb5b049ea88074ade4e5b38a0e96a9
CharukaK <charukakarunanayake@gmail.com>
2024-05-28 22:13:51 +0530

    Add upkey change state

--- internal/state/state.go
+++ internal/state/state.go
@@ -1,9 +1,57 @@
 package state
 
+import (
+	"container/list"
+	"log"
+)
+
+type MoveEvent int
+
+const (
+	MoveUp MoveEvent = iota
+	MoveDown
+	MoveLeft
+	MoveRight
+)
+
 type GameState struct {
 	Board [][]int
 }
 
+func (gs *GameState) MakeMove(ev MoveEvent) {
+	switch ev {
+	case MoveUp:
+		for col := 0; col < len(gs.Board); col++ {
+			var colList list.List
+			for row := 0; row < len(gs.Board)-1; row++ {
+				if gs.Board[row][col] != 0 {
+					colList.PushBack(gs.Board[row][col])
+				}
+
+				gs.Board[row][col] = 0
+			}
+
+			ri := 0
+			for x := colList.Front(); x != nil; x = x.Next() {
+				next := x.Next()
+				val, ok := x.Value.(int)
+				if !ok {
+					log.Fatal("error parsing board value")
+				}
+				if next != nil && x.Value == next.Value {
+					gs.Board[ri][col] = val * 2
+					x = next
+				} else {
+					gs.Board[ri][col] = val
+				}
+				ri++
+			}
+		}
+	case MoveDown:
+	case MoveRight:
+	case MoveLeft:
+	}
+}
 
 func NewGameState(size int) *GameState {
 	board := make([][]int, size)
